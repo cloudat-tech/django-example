2bf06d0c8abe384ed655d9082ebc9d5f95cb9d59
cloudat-tech <122144107+cloudat-tech@users.noreply.github.com>
2023-03-14 12:36:09 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
+#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
