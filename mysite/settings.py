e9fe99013bbe94be61f9a3d2259712ea5fae15ad
cloudat-tech <labs.cloudat.tech@outlook.com>
2023-03-14 12:10:29 +0530

    setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
- DATABASES = {
+DATABASES = {
    'default':{
       'ENGINE': 'django.db.backends.postgresql_psycopg2',
       'NAME': os.getenv('DATABASE_NAME'),
