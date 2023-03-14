d2c9099941f88c8fc99f017c502a1ebe58a5141a
cloudat-tech <122144107+cloudat-tech@users.noreply.github.com>
2023-03-14 12:27:47 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,8 +133,11 @@ AWS_QUERYSTRING_AUTH = False
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = 'static/'
+#STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+#STATIC_ROOT = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Logging Configuration
 
